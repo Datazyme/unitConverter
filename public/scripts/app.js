f9ec24de4a7e00cf65b7a90969558d534aa5b476
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-08-26 19:11:30 -0700

    can take user input, apply javascript function and display result in the text box

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,30 @@
 // Client facing scripts here
+const tempConverter = function (value) {
+  let celsius = Math.round(((value - 32) / 1.8) * 10) / 10;
+  return celsius;
+};
+
+// const tempConverter = function (value, cToF) {
+//   if (typeof value === "string") {
+//     return NaN;
+//   }
+
+//   if (cToF === true) {
+//     let farenheit = Math.round(((value * 9) / 5 + 32) * 10) / 10;
+//     return farenheit;
+//   } else if (cToF === false) {
+//     let celsius = Math.round(((value - 32) / 1.8) * 10) / 10;
+//     return celsius;
+//   }
+// };
+
+// console.log(tempConverter(42, false));
+
+$(document).ready(function () {
+  $("#submitButton").click(function (event) {
+    event.preventDefault();
+    $(".tweet").val(tempConverter($(".tweet").val()));
+    //console.log(tempConverter($(".tweet").val(event)));
+    //console.log("works");
+  });
+});
