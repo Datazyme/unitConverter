e98e89be94cead26cf982d5116989c3edc8ea1f5
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-09-04 18:18:41 -0700

    before making drastic changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,103 @@
 // Client facing scripts here
-const tempConverter = function (value) {
-  let celsius = Math.round(((value - 32) / 1.8) * 10) / 10;
-  return celsius;
+
+//target all the necessary HTML elements with unique identifiers
+const fromFarenheit = document.querySelector("#from-farenheit");
+const fromCelsius = document.querySelector("#from-celsius");
+const toFarenheit = document.querySelector("#to-farenheit");
+const toCelsius = document.querySelector("#to-celsius");
+const submitBtn = document.querySelector("#submit-button");
+
+// Listen for checkbox ticked and length
+fromFarenheit.addEventListener("change", function () {
+  if (this.checked) {
+    console.log("fromFarenheit is checked..");
+  } else {
+    console.log("fromFarenheit is unchecked..");
+  }
+});
+
+fromCelsius.addEventListener("change", function () {
+  if (this.checked) {
+    console.log("fromCelsius is checked..");
+  } else {
+    console.log("fromCelsius is unchecked..");
+  }
+});
+
+toFarenheit.addEventListener("change", function () {
+  if (this.checked) {
+    console.log("toFarenheit is checked..");
+  } else {
+    console.log("toFarenheit is unchecked..");
+  }
+});
+
+toCelsius.addEventListener("change", function () {
+  if (this.checked) {
+    console.log("toCelsius is checked..");
+  } else {
+    console.log("toCelsius is unchecked..");
+  }
+});
+
+// submitBtn.addEventListener("click", () => {
+const fromFartoCels = fromFarenheit.checked && toCels.checked;
+const fromCelstoFar = fromCelsius.checked && toFar.checked;
+
+let checking = function () {
+  if (fromFartoCels) {
+    console.log("checking");
+  }
 };
+//   result.value = tempConverter(value, fromFartoCels, fromCelstoFar);
+// });
 
-// const tempConverter = function (value, cToF) {
-//   if (typeof value === "string") {
-//     return NaN;
-//   }
-
-//   if (cToF === true) {
-//     let farenheit = Math.round(((value * 9) / 5 + 32) * 10) / 10;
-//     return farenheit;
-//   } else if (cToF === false) {
-//     let celsius = Math.round(((value - 32) / 1.8) * 10) / 10;
-//     return celsius;
+// const cToF = function () {
+//   if (fromCelsius.checked && toFarenheit.checked) {
+//     return true;
+//   } else if (fromFarenheit.checked && toCelsius.checked) {
+//     return false;
+//   } else {
+//     console.log("not a valid choice");
 //   }
 // };
 
-// console.log(tempConverter(42, false));
+//convert temperatures function
+const farenToCel = function (value) {
+  let celsius = Math.round(((value - 32) / 1.8) * 10) / 10;
+  return celsius;
+};
+const CelcToFaren = function (value) {
+  let farenheit = Math.round(((value * 9) / 5 + 32) * 10) / 10;
+  return farenheit + "F";
+};
 
+// const tempConverter = function convert(value, fromFartoCels, fromCelstoFar) {
+//   // if (typeof value === "string") {
+//   //   return typeof value;
+//   // }
+
+//   if (fromCelstoFar) {
+//     let farenheit = Math.round(((value * 9) / 5 + 32) * 10) / 10;
+//     return farenheit + "F";
+//   } else if (fromFartoCels) {
+//     let celsius = Math.round(((value - 32) / 1.8) * 10) / 10;
+//     return celsius + "C";
+//   }
+// };
+
+//console.log(tempConverter(42, false));
+
+//allows user to enter number into textbox, applies function and spits out value to same box
 $(document).ready(function () {
   $("#submitButton").click(function (event) {
     event.preventDefault();
-    $(".tweet").val(tempConverter($(".tweet").val()));
-    //console.log(tempConverter($(".tweet").val(event)));
-    //console.log("works");
+    if (fromFartoCels) {
+      $(".number").val(farenToCel($(".number").val()));
+    } else if (fromCelstoFar) {
+      $(".number").val(CelcToFaren($(".number").val()));
+    }
+    //console.log($("#number-text").val(tempConverter($("#number-text").val())));
+    console.log("works");
   });
 });
