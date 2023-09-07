3dbfdfdf99b2b178a50c5e2ab077822cb6986208
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-09-07 16:33:46 -0700

    working temp converter, database components removed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,103 +1,63 @@
 // Client facing scripts here
 
-//target all the necessary HTML elements with unique identifiers
-const fromFarenheit = document.querySelector("#from-farenheit");
-const fromCelsius = document.querySelector("#from-celsius");
-const toFarenheit = document.querySelector("#to-farenheit");
-const toCelsius = document.querySelector("#to-celsius");
-const submitBtn = document.querySelector("#submit-button");
-
-// Listen for checkbox ticked and length
-fromFarenheit.addEventListener("change", function () {
-  if (this.checked) {
-    console.log("fromFarenheit is checked..");
-  } else {
-    console.log("fromFarenheit is unchecked..");
-  }
-});
-
-fromCelsius.addEventListener("change", function () {
-  if (this.checked) {
-    console.log("fromCelsius is checked..");
-  } else {
-    console.log("fromCelsius is unchecked..");
-  }
-});
-
-toFarenheit.addEventListener("change", function () {
-  if (this.checked) {
-    console.log("toFarenheit is checked..");
-  } else {
-    console.log("toFarenheit is unchecked..");
-  }
-});
-
-toCelsius.addEventListener("change", function () {
-  if (this.checked) {
-    console.log("toCelsius is checked..");
-  } else {
-    console.log("toCelsius is unchecked..");
-  }
-});
-
-// submitBtn.addEventListener("click", () => {
-const fromFartoCels = fromFarenheit.checked && toCels.checked;
-const fromCelstoFar = fromCelsius.checked && toFar.checked;
-
-let checking = function () {
-  if (fromFartoCels) {
-    console.log("checking");
-  }
-};
-//   result.value = tempConverter(value, fromFartoCels, fromCelstoFar);
-// });
-
-// const cToF = function () {
-//   if (fromCelsius.checked && toFarenheit.checked) {
-//     return true;
-//   } else if (fromFarenheit.checked && toCelsius.checked) {
-//     return false;
-//   } else {
-//     console.log("not a valid choice");
-//   }
-// };
-
 //convert temperatures function
-const farenToCel = function (value) {
-  let celsius = Math.round(((value - 32) / 1.8) * 10) / 10;
+const farenToCel = function (inputValue) {
+  let celsius = Math.round(((inputValue - 32) / 1.8) * 10) / 10;
   return celsius;
 };
-const CelcToFaren = function (value) {
-  let farenheit = Math.round(((value * 9) / 5 + 32) * 10) / 10;
-  return farenheit + "F";
+const celsToFaren = function (inputValue) {
+  let farenheit = Math.round(((inputValue * 9) / 5 + 32) * 10) / 10;
+  return farenheit;
 };
-
-// const tempConverter = function convert(value, fromFartoCels, fromCelstoFar) {
-//   // if (typeof value === "string") {
-//   //   return typeof value;
-//   // }
-
-//   if (fromCelstoFar) {
-//     let farenheit = Math.round(((value * 9) / 5 + 32) * 10) / 10;
-//     return farenheit + "F";
-//   } else if (fromFartoCels) {
-//     let celsius = Math.round(((value - 32) / 1.8) * 10) / 10;
-//     return celsius + "C";
-//   }
-// };
-
-//console.log(tempConverter(42, false));
 
 //allows user to enter number into textbox, applies function and spits out value to same box
 $(document).ready(function () {
-  $("#submitButton").click(function (event) {
-    event.preventDefault();
-    if (fromFartoCels) {
-      $(".number").val(farenToCel($(".number").val()));
-    } else if (fromCelstoFar) {
-      $(".number").val(CelcToFaren($(".number").val()));
+  $("#convertBtn").click(function () {
+    // Get user input values
+    const inputValue = parseFloat($("#inputValue").val());
+    console.log(inputValue);
+    const fromUnit = $("#fromUnit").val();
+    const toUnit = $("#toUnit").val();
+    const farenToCel = function (value) {
+      let celsius = Math.round(((value - 32) / 1.8) * 10) / 10;
+      return celsius;
+    };
+    const celsToFaren = function (value) {
+      let farenheit = Math.round(((value * 9) / 5 + 32) * 10) / 10;
+      return farenheit;
+    };
+    const celsToKelvin = function (value) {
+      let kelvin = Math.round((value + 273) * 10) / 10;
+      return kelvin;
+    };
+    const kelvToCels = function (value) {
+      let kelvin = Math.round((value - 273) * 10) / 10;
+      return kelvin;
+    };
+    const farentToKelv = function (value) {
+      let kelvin = (Math.round(((value - 32) * 5) / 9 + 273) * 10) / 10;
+      return kelvin;
+    };
+
+    // Perform conversion
+    let result;
+    if (fromUnit === "F" && toUnit === "C") {
+      result = farenToCel(inputValue);
+    } else if (fromUnit === "C" && toUnit === "F") {
+      result = celsToFaren(inputValue);
+    } else if (fromUnit === "C" && toUnit === "K") {
+      result = celsToKelvin(inputValue);
+    } else if (fromUnit === "K" && toUnit === "C") {
+      result = kelvToCels(inputValue);
+    } else if (fromUnit === "F" && toUnit === "K") {
+      result = farentToKelv(inputValue);
+    } else {
+      result = inputValue; // Same units, no conversion needed
     }
-    //console.log($("#number-text").val(tempConverter($("#number-text").val())));
-    console.log("works");
+
+    //Display the result
+    $("#result").text(
+      `${inputValue}${fromUnit} is equal to ${result.toFixed(2)}${toUnit}`
+    );
   });
 });
