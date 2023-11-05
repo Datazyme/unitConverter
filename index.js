fc17f5a319fad642eb63c46f236c920f168d4978
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-11-05 10:59:13 -0800

    change order of app use

--- index.js
+++ index.js
@@ -12,20 +12,19 @@ const app = express();
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for  error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   "/styles",
-//   sassMiddleware({
-//     source: __dirname + "/styles",
-//     destination: __dirname + "/public/styles",
-//     isSass: false, // false => scss, true => sass
-//   })
-// );
-
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  "/styles",
+  sassMiddleware({
+    source: __dirname + "/styles",
+    destination: __dirname + "/public/styles",
+    isSass: false, // false => scss, true => sass
+  })
+);
 
 // Home page
 // Warning: avoid creating more routes in this file!
